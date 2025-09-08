c1227409fb2840a19772cee9314123157b3063a3
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-09-08 22:35:31 +0530

    feat: update layout and home page, add user page, and style adjustments

---
+++ abhi-portfolio/app/user/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function UserPage() {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold text-center">User Page</h1>
+            {/* Add your user page content here */}
+            <Link href={'/'}>Home</Link>
+        </div>
+    );
+}
