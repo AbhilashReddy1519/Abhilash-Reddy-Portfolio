5bb7c71b564366cfb06aea5cbdc1f30a8f9206f6
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-09-10 13:37:05 +0530

    Refactor code structure for improved readability and maintainability
    feat: Added preloader when page is visited

---
+++ abhi-portfolio/app/components/load.jsx
@@ -0,0 +1,80 @@
+"use client";
+import { useState, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import styles from "./load.module.css";
+
+gsap.registerPlugin(useGSAP);
+
+const Load = () => {
+    const [loading, setLoading] = useState(false);
+
+    useGSAP(() => {
+        const tl = gsap.timeline();
+
+        // Animate each name with different scales and timing
+        tl.from(".name-1", {
+            scale: 10,
+            opacity: 0,
+            duration: 0.5,
+            filter: "blur(10px)",
+            ease: "power4.out",
+        })
+            .from(
+                ".name-2",
+                {
+                    scale: 10,
+                    opacity: 0,
+                    duration: 0.5,
+                    filter: "blur(10px)",
+                    ease: "power4.out",
+                },
+                "-=0.4"
+            ) // Start slightly before previous ends
+            .from(
+                ".name-3",
+                {
+                    filter: "blur(10px)",
+                    scale: 10,
+                    opacity: 0,
+                    duration: 0.5,
+                    ease: "power4.out",
+                },
+                "-=0.4"
+            )
+            .from(
+                ".name-4",
+                {
+                    filter: "blur(10px)",
+                    scale: 10,
+                    opacity: 0,
+                    duration: 0.5,
+                    ease: "power4.out",
+                },
+                "-=0.4"
+            );
+        
+
+        gsap.from("#image", {
+            duration: 1,
+            y: 1000,
+            opacity: 1,
+            ease: "power4.out",
+        });
+    });
+
+    return (
+        <>
+            <section className={styles.container}>
+                <div id="image" className={styles.loadMain}>
+                    <h1 className={`${styles.name} name-1`}>Abhilash Reddy</h1>
+                    <h1 className={`${styles.name} name-2`}>Abhilash Reddy</h1>
+                    <h1 className={`${styles.name} name-3`}>Abhilash Reddy</h1>
+                    <h1 className={`${styles.name} name-4`}>Abhilash Reddy</h1>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Load;
--- abhi-portfolio/app/layout.tsx
+++ abhi-portfolio/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Load from './components/load';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Abhilash Reddy</h1>
+        <Load></Load>
         {children}
       </body>
     </html>
