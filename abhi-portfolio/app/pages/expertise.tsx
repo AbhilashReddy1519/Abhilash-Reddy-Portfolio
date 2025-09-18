410d825bf154cc19c82afd879847333350ddd923
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-09-18 11:47:09 +0530

    `feat: updated nav styles, added images to expertise page, and modified main page layout`

--- abhi-portfolio/app/pages/expertise.tsx
+++ abhi-portfolio/app/pages/expertise.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './main.module.css';
+import Image from "next/image";
 
 const Expertise = () => {
     return (
@@ -8,7 +9,15 @@ const Expertise = () => {
                 <h1>My Expertise</h1>
                 <div className={styles.expertContain}>
                     <div className={styles.expertDiv1}>
-                        <h2>Software Engineering & Algorithms</h2>
+                        <div>
+                            <Image
+                                src={"/icons/pc.png"}
+                                alt="pc image"
+                                width={"60"}
+                                height="60"
+                            />
+                            <h2>Software Engineering</h2>
+                        </div>
                         <h3>
                             Proficient in Java, C++, and Python with strong
                             foundations in data structures, algorithms, and
@@ -16,7 +25,15 @@ const Expertise = () => {
                         </h3>
                     </div>
                     <div className={styles.expertDiv2}>
-                        <h2>System & Full-Stack Development</h2>
+                        <div>
+                            <Image
+                                src={"/icons/react.png"}
+                                alt="react image"
+                                width={"60"}
+                                height="60"
+                            />
+                            <h2>Full-Stack Dev System Design</h2>
+                        </div>
                         <h3>
                             Hands-on in building scalable applications with
                             React, Next.js, Node.js, Express, and MongoDB, plus
@@ -25,7 +42,15 @@ const Expertise = () => {
                         </h3>
                     </div>
                     <div className={styles.expertDiv3}>
-                        <h2>Data Engineering & Cloud Systems</h2>
+                        <div>
+                            <Image
+                                src={"/icons/data.png"}
+                                alt="data image"
+                                width={"60"}
+                                height="60"
+                            />
+                            <h2>Data Eng. & Cloud Systems</h2>
+                        </div>
                         <h3>
                             Pursuing IBM Data Engineering Professional
                             Certificate, experienced with SQL, ETL pipelines,
@@ -33,6 +58,12 @@ const Expertise = () => {
                             architectures.
                         </h3>
                     </div>
+                    <Image 
+                        src={"/expertise.png"}
+                        alt="Simple comedy backGround"
+                        height={1000}
+                        width={1000}
+                    />
                 </div>
             </section>
         </>
