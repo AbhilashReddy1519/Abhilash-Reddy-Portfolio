5110db35950732826c675df0beaefb660114a34e
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-11-06 13:43:54 +0530

    `Update layout and responsive module to include viewport settings`

--- abhi-portfolio/app/layout.tsx
+++ abhi-portfolio/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -16,7 +16,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "Abhilash Reddy",
 	description: "This is my portfolio",
-	viewport: "width=device-width, initial-scale=1",
+	// viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	// Add other viewport settings here (e.g., maximumScale, themeColor)
 };
 
 export default function RootLayout({
