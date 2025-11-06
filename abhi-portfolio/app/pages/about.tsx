69bc349e817f23f801e87d7d4b8da173d367408f
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-11-06 13:16:52 +0530

    `Fixed CSS styles for ballpit and about page overlay`

--- abhi-portfolio/app/pages/about.tsx
+++ abhi-portfolio/app/pages/about.tsx
@@ -74,7 +74,7 @@ const About = () => {
 						left: 0,
 						right: 0,
 						bottom: 0,
-						width: "100%",
+						// width: "100%",
 						zIndex: "-1",
                         pointerEvents:"none",
 					}}>
